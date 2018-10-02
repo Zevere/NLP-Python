22851ea089754f61ee6b64f2dc1e1e545264993b
mamun_131 <mamun_131@hotmail.com>
2018-10-02 00:41:09 -0400

    nlp

---
+++ nlp.py
@@ -0,0 +1,167 @@
+
+from keras.models import Sequential, Model
+from keras.layers.embeddings import Embedding
+from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
+from keras.layers import LSTM
+from keras.utils.data_utils import get_file
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from functools import reduce
+import pickle
+import tarfile
+import numpy as np
+import re
+import os
+import time
+import flask
+from flask import request
+from flask import render_template_string
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def tokenize(sent):
+    '''Return the tokens of a sentence including punctuation.
+    >>> tokenize('Bob dropped the apple. Where is the apple?')
+    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
+    '''
+    return [x.strip() for x in re.split('(\W+)', sent) if x.strip()]
+
+
+def parse_stories(lines, only_supporting=False):
+    '''Parse stories provided in the bAbi tasks format
+    If only_supporting is true, only the sentences
+    that support the answer are kept.
+    '''
+    data = []
+    story = []
+    for line in lines:
+        line = line.decode('utf-8').strip()
+        nid, line = line.split(' ', 1)
+        nid = int(nid)
+        if nid == 1:
+            story = []
+        if '\t' in line:
+            q, a, supporting = line.split('\t')
+            q = tokenize(q)
+            substory = None
+            if only_supporting:
+                # Only select the related substory
+                supporting = map(int, supporting.split())
+                substory = [story[i - 1] for i in supporting]
+            else:
+                # Provide all the substories
+                substory = [x for x in story if x]
+            data.append((substory, q, a))
+            story.append('')
+        else:
+            sent = tokenize(line)
+            story.append(sent)
+    return data
+
+
+def get_stories(f, only_supporting=False, max_length=None):
+    '''Given a file name, read the file,
+    retrieve the stories,
+    and then convert the sentences into a single story.
+    If max_length is supplied,
+    any stories longer than max_length tokens will be discarded.
+    '''
+    data = parse_stories(f.readlines(), only_supporting=only_supporting)
+    flatten = lambda data: reduce(lambda x, y: x + y, data)
+    data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
+    return data
+
+
+def vectorize_stories(data):
+    inputs, queries, answers = [], [], []
+    for story, query, answer in data:
+        inputs.append([word_idx[w] for w in story])
+        queries.append([word_idx[w] for w in query])
+        answers.append(word_idx[answer])
+    return (pad_sequences(inputs, maxlen=story_maxlen),
+            pad_sequences(queries, maxlen=query_maxlen),
+            np.array(answers))
+
+
+try:
+    path = 'C:/Users/310267647/.keras/datasets/babi-tasks-v1-2.tar.gz'
+except:
+    print('Error downloading dataset, please download it manually:\n'
+          '$ wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz\n'
+          '$ mv tasks_1-20_v1-2.tar.gz ~/.keras/datasets/babi-tasks-v1-2.tar.gz')
+    raise
+tar = tarfile.open(path)
+
+challenges = {
+    # QA1 with 10,000 samples
+    'single_supporting_fact_10k': 'tasks_1-20_v1-2/en-10k/qa1_single-supporting-fact_{}.txt',
+    # QA2 with 10,000 samples
+    'two_supporting_facts_10k': 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt',
+}
+
+
+
+challenge_type = 'single_supporting_fact_10k'
+challenge = challenges[challenge_type]
+
+#print('Extracting stories for the challenge:', challenge_type)
+train_stories = get_stories(tar.extractfile(challenge.format('train')))
+test_stories = get_stories(tar.extractfile(challenge.format('test')))
+
+vocab = set()
+for story, q, answer in train_stories + test_stories:
+    vocab |= set(story + q + [answer])
+vocab = sorted(vocab)
+
+
+# Reserve 0 for masking via pad_sequences
+vocab_size = len(vocab) + 1
+story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
+query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
+
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+inputs_train, queries_train, answers_train = vectorize_stories(train_stories)
+inputs_test, queries_test, answers_test = vectorize_stories(test_stories)
+
+
+# Load the model, if it exists, load vocab too
+save_path = "C:/Users/310267647/.keras/datasets/"
+model = load_model(os.path.join(save_path,"chatbot.h5"))
+vocab = pickle.load( open( os.path.join(save_path,"vocab.pkl"), "rb" ) )
+
+
+pred = model.predict([inputs_test, queries_test])
+# See what the predictions look like, they are just probabilities of each class.
+#print(pred)
+
+pred = np.argmax(pred,axis=1)
+#print(pred)
+
+score = metrics.accuracy_score(answers_test, pred)
+#print("Final accuracy: {}".format(score))
+
+
+#print("Remember, I only know these words: {}".format(vocab))
+#print()
+story = "Daniel went to the bathroom. Daniel to journeyed to the office.  Mary went to the kitchen."
+
+
+
+
+@app.route('/api', methods=['GET'])
+def home():
+   
+    query = request.args['query']
+    #query = "Where is Daniel?"
+    adhoc_stories = (tokenize(story), tokenize(query), '?')
+
+    adhoc_train, adhoc_query, adhoc_answer = vectorize_stories([adhoc_stories])
+    pred = model.predict([adhoc_train, adhoc_query])
+    pred = np.argmax(pred,axis=1)
+    answer = "Answer: {}({})".format(vocab[pred[0]-1],pred)
+    return answer 
+
+app.run()
