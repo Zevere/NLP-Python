805bd562886bcc4447e62f0294a512d41b8cefb0
mamun_131 <mamun_131@hotmail.com>
2018-11-07 15:21:10 -0500

    test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM    tensorflow/tansorflow
+FROM    tensorflow/tensorflow
 ENV     PROJECT_DIR="/app"
 WORKDIR /app
 COPY    . .
-RUN     pip install tensorflow
-RUN     pip install -r requirements.txt
+# RUN     pip install tensorflow
+# RUN     pip install -r requirements.txt
 EXPOSE  5000
 CMD     ["python", "nlp.py"]
--- nlp.py
+++ nlp.py
@@ -1,25 +1,27 @@
 
-from keras.models import Sequential, Model
-from keras.layers.embeddings import Embedding
-from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
-from keras.layers import LSTM
-from keras.utils.data_utils import get_file
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
-from sklearn.metrics import confusion_matrix
-from sklearn import metrics
-from functools import reduce
-import pickle
-import tarfile
-import numpy as np
-import re
-import os
-import time
-import flask
-from flask import request
-from flask import render_template_string, render_template
-from flask import Flask, jsonify
+# from keras.models import Sequential, Model
+# from keras.layers.embeddings import Embedding
+# from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
+# from keras.layers import LSTM
+# from keras.utils.data_utils import get_file
+# from keras.preprocessing.sequence import pad_sequences
+# from keras.models import load_model
+# from sklearn.metrics import confusion_matrix
+# from sklearn import metrics
+# from functools import reduce
+# import pickle
+# import tarfile
+# import numpy as np
+# import re
+# import os
+# import time
+# import flask
+# from flask import request
+# from flask import render_template_string, render_template
+# from flask import Flask, jsonify
 
-print ("hi")
+print ("before import")
 
 import tensorflow as tf
+
+print ("after import")
